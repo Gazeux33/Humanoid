62796660999e91cbb47d9f090da27c05c2ad4779
Theo <thoecastillo@yahoo.com>
2025-01-27 21:54:56 +0100

    refactor: add argument parsing for training and visualization commands

--- main.py
+++ main.py
@@ -1,26 +1,31 @@
+import argparse
 from src.simulation import DefaultHumanoidSimulation
 from src.utils import get_last_save_path
 
-
-
-
-
-
 ENV_NAME = 'Humanoid-v5'
 
+def parse_arguments():
+    parser = argparse.ArgumentParser(description="Humanoid Simulation")
+    subparsers = parser.add_subparsers(dest='command', required=True)
+
+    train_parser = subparsers.add_parser('train', help='Train the humanoid simulation')
+    train_parser.add_argument('--checkpoints', type=str, default='checkpoints/DefaultHumanoid', help='Path to checkpoints')
+    train_parser.add_argument('--save_freq', type=int, default=50, help='Save frequency')
+    train_parser.add_argument('--max_episodes', type=int, default=1000, help='Maximum number of episodes')
+
+    visualize_parser = subparsers.add_parser('visualize', help='Visualize the humanoid simulation')
+    visualize_parser.add_argument('--path_to_model', type=str, required=True, help='Path to the model')
+    visualize_parser.add_argument('--iterations', type=int, default=10, help='Number of iterations')
+
+    return parser.parse_args()
+
 if __name__ == "__main__":
+    args = parse_arguments()
     name = "DefaultHumanoid"
     simulation = DefaultHumanoidSimulation(name)
-    last_save_path = get_last_save_path('checkpoints/DefaultHumanoid')
-    #simulation.train(checkpoints=last_save_path, save_freq=50)
-    simulation.visualize(path_to_model=last_save_path, iterations=10)
 
-
-
-
-
-
-
-
-
-
+    if args.command == 'train':
+        last_save_path = get_last_save_path(args.checkpoints)
+        simulation.train(checkpoints=last_save_path, save_freq=args.save_freq, max_episodes=args.max_episodes)
+    elif args.command == 'visualize':
+        simulation.visualize(path_to_model=args.path_to_model, iterations=args.iterations)

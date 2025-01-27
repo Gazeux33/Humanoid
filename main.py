8f9cc0747a9e1bfa5c92b0c796339d242826b2c2
tcastillo-ubuntu <theocastillo@yahoo.com>
2025-01-27 19:54:47 +0100

    refactor main.py to dynamically retrieve the latest checkpoint for visualization

--- main.py
+++ main.py
@@ -1,16 +1,25 @@
+import os
+
 import glfw
 
 from simulations import DefaultHumanoidSimulation
 
 glfw.init()
 
+def get_last(path:str)-> str:
+    return os.path.join(path,max(os.listdir(path), key=lambda x: int(x.split('_')[1].split('.')[0])).replace('.zip', ''))
+
+
 ENV_NAME = 'Humanoid-v5'
 
 if __name__ == "__main__":
     name = "DefaultHumanoid"
     simulation = DefaultHumanoidSimulation(name)
-    # simulation.train()
-    simulation.visualize(path_to_model=f"checkpoints/{name}/SAC_150", iterations=10)
+    last_save_path = get_last('checkpoints/DefaultHumanoid')
+    #simulation.train(checkpoints=last_save_path, save_freq=50)
+    simulation.visualize(path_to_model=last_save_path, iterations=10)
+
+
 
 
 
